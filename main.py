631f48e3fe9330c3f7c1c29e899a07507a019498
PatrykNielacny <100844107+PatrykNielacny@users.noreply.github.com>
2022-03-17 10:37:39 +0000

    code

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-print("Podaj liczbe: ")
-liczba = int(input())
-if(liczba%2==0):
-  print("Podana liczba jest parzysta")
-else:
-  print("Podana liczba nie jest parzysta")
+import math
+# Zadanie 4
+
+#print("Podaj liczbe: ")
+#liczba = int(input())
+#if(liczba%2==0):
+#  print("Podana liczba jest parzysta")
+#else:
+#  print("Podana liczba nie jest parzysta")
+
+# Zadanie 5
+
+#print("Podaj liczbe n: ")
+#n = int(input())
+#suma = 0
+#for n in range (1,n):
+#  suma += 1/n
+#print(suma)
+
+# Zadanie 6
+
+#i = 1
+#while(i != 10):
+# print(math.sqrt(i))
+#  i += 1
